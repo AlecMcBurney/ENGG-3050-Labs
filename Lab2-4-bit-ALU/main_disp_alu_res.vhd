aa393d7408f1ca46ed5e809895b1e40d3f24d8ec
alecmcburney <alecmcburney@gmail.com>
2026-09-21 12:42:41 -0400

    Folder name changes + init lab2

---
+++ Lab2-4-bit-ALU/main_disp_alu_res.vhd
@@ -0,0 +1,60 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity display_alu_result is
+    Port (
+        SW: in std_logic_vector(11 downto 0); -- use 12 switches as binary bits
+        DIG_SEL : in std_logic_vector(2 downto 0); -- number to select anode based on clock 
+        ANODES : out std_logic_vector(7 downto 0); --7 seg ANODES
+        SEG_CATHODES : out std_logic_vector(7 downto 0) --7 seg Cathodes
+	);
+end display_alu_result;
+
+architecture Structural of display_alu_result is
+
+	--Component Declarations:
+
+    -- 6-bit adder
+    component sixbitadder
+        Port ( 
+            A, B   : in  std_logic_vector(5 downto 0);
+            Carry  : out std_logic;
+            Sum : out std_logic_vector(5 downto 0)
+        );
+    end component;
+    
+    -- Seven segment display logic
+    component sevseg_disp
+        Port (
+            VAL : in STD_LOGIC_VECTOR (5 downto 0);
+            AN_SEL : in std_logic_vector(2 downto 0);
+            AN : out std_logic_vector(7 downto 0);
+            SEG : out STD_LOGIC_VECTOR (7 downto 0)
+        );
+    end component;
+    
+    -- Signal Declaration
+    -- Signals for 6b adder outputs
+    signal res : std_logic_vector(5 downto 0);
+    signal carry: std_logic;
+
+begin
+
+	adder: sixbitadder
+	    port map(
+            A => SW(11 downto 6),
+            B => SW(5 downto 0),
+            Carry => carry,
+            Sum => res
+        );
+    
+    disp: sevseg_disp
+        port map (
+            VAL => res,
+            AN_SEL => "000",
+            AN => ANODES,
+            SEG => SEG_CATHODES
+        );
+end Structural;
---
+++ Lab2-4-bit-ALU/sevseg_1.vhd
@@ -0,0 +1,37 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity sevseg_dot is
+    Port ( 
+        int : in STD_LOGIC_VECTOR (4 downto 1);-- same input vector here
+        dot : in std_logic;		-- as here when using component. 4-1 bits are character in hex, 0 bit is dot
+        seg : out STD_LOGIC_VECTOR (7 downto 0) -- 6-0 bits are cathodes, 7th bit is dot. -- "0" is on, "1" is off
+    );
+end sevseg_dot;
+
+architecture Behavioral of sevseg_dot is
+begin
+    decode: process(int, dot)
+    begin
+        case (int) is
+            when "0000" => seg <= dot &"1000000" ;--& dot; 	--0 --DOT,G,F,E,D,C,B,A,
+            when "0001" => seg <= dot &"1111001" ;--& dot; 	--1
+            when "0010" => seg <= dot &"0100100" ;--& dot; 	--2
+            when "0011" => seg <= dot &"0110000" ;--& dot; 	--3
+            when "0100" => seg <= dot &"0011001" ;--& dot; 	--4
+            when "0101" => seg <= dot &"0010010" ;--& dot; 	--5
+            when "0110" => seg <= dot &"0000010" ;--& dot; 	--6
+            when "0111" => seg <= dot &"1111000" ;--& dot; 	--7
+            when "1000" => seg <= dot &"0000000" ;--& dot;  --8
+            when "1001" => seg <= dot &"0011000" ;--& dot; 	--9
+            when "1010" => seg <= dot &"0001000" ;--& dot;	--A
+            when "1011" => seg <= dot &"0000011" ;--& dot;	--b
+            when "1100" => seg <= dot &"1000110" ;--& dot;	--C
+            when "1101" => seg <= dot &"0100001" ;--& dot;	--d
+            when "1110" => seg <= dot &"0000110" ;--& dot;	--E
+            when "1111" => seg <= dot &"0001110" ;--& dot;	--F
+            when others => seg <= dot &"1111110" ;--& dot;	---
+        end case;
+    end process;
+
+end Behavioral;
